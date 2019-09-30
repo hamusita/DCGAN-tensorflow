9d6a822a13b349ce9ac28e54510f0340b9508309
hamusita <meazero965@gmail.com>
2019-09-30 13:30:08 +0900

    add comment to main.py
    exit()

--- main.py
+++ main.py
@@ -7,44 +7,51 @@ from utils import pp, visualize, to_json, show_all_variables
 
 import tensorflow as tf
 
+#初期パラメーターの定義
 flags = tf.app.flags
-flags.DEFINE_integer("epoch", 25, "Epoch to train [25]")
-flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
-flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
-flags.DEFINE_float("train_size", np.inf, "The size of train images [np.inf]")
-flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
-flags.DEFINE_integer("input_height", 108, "The size of image to use (will be center cropped). [108]")
-flags.DEFINE_integer("input_width", None, "The size of image to use (will be center cropped). If None, same value as input_height [None]")
-flags.DEFINE_integer("output_height", 64, "The size of the output images to produce [64]")
-flags.DEFINE_integer("output_width", None, "The size of the output images to produce. If None, same value as output_height [None]")
-flags.DEFINE_string("dataset", "celebA", "The name of dataset [celebA, mnist, lsun]")
-flags.DEFINE_string("input_fname_pattern", "*.jpg", "Glob pattern of filename of input images [*]")
-flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
-flags.DEFINE_string("data_dir", "./data", "Root directory of dataset [data]")
-flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image samples [samples]")
-flags.DEFINE_boolean("train", False, "True for training, False for testing [False]")
-flags.DEFINE_boolean("crop", False, "True for training, False for testing [False]")
-flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]")
-flags.DEFINE_integer("generate_test_images", 100, "Number of images to generate during test. [100]")
+flags.DEFINE_integer("epoch", 25, "Epoch to train [25]") #エポックのサイズ
+flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]") #adamから持ってきた学習率
+flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]") #adamの運動量項
+flags.DEFINE_float("train_size", np.inf, "The size of train images [np.inf]") #訓練データのサイズ
+flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]") #バッチのサイズ
+flags.DEFINE_integer("input_height", 108, "The size of image to use (will be center cropped). [108]") #インプット画像の高さ
+flags.DEFINE_integer("input_width", None, "The size of image to use (will be center cropped). If None, same value as input_height [None]") #インプット画像の幅
+flags.DEFINE_integer("output_height", 64, "The size of the output images to produce [64]") #生成する画像の高さ
+flags.DEFINE_integer("output_width", None, "The size of the output images to produce. If None, same value as output_height [None]") #インプット画像の幅
+flags.DEFINE_string("dataset", "celebA", "The name of dataset [celebA, mnist, lsun]") #データセットの名前
+flags.DEFINE_string("input_fname_pattern", "*.jpg", "Glob pattern of filename of input images [*]") #入力画像のパターン
+flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]") #チェックポイントの保存先のディレクトリ
+flags.DEFINE_string("data_dir", "./data", "Root directory of dataset [data]") #データセットのルートディレクトリ
+flags.DEFINE_string("sample_dir", "samples", "Directory name to save the image samples [samples]") #サンプル画像の保存先
+flags.DEFINE_boolean("train", False, "True for training, False for testing [False]") #トレーニングか否か
+flags.DEFINE_boolean("crop", False, "True for training, False for testing [False]") #トレーニングに対してTrue、テストに対してFalse
+flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]") #可視化する場合True
+flags.DEFINE_integer("generate_test_images", 100, "Number of images to generate during test. [100]") #テスト中に生成する画像の数
 FLAGS = flags.FLAGS
 
+#出力先ディレクトリの作成
 def main(_):
+  #入力画像のサイズ補完
   pp.pprint(flags.FLAGS.__flags)
 
+  #入力画像のサイズ補完
   if FLAGS.input_width is None:
     FLAGS.input_width = FLAGS.input_height
   if FLAGS.output_width is None:
     FLAGS.output_width = FLAGS.output_height
 
+  #出力先ディレクトリの作成
   if not os.path.exists(FLAGS.checkpoint_dir):
     os.makedirs(FLAGS.checkpoint_dir)
   if not os.path.exists(FLAGS.sample_dir):
     os.makedirs(FLAGS.sample_dir)
 
+  #gpuの使用設定
   #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
   run_config = tf.ConfigProto()
   run_config.gpu_options.allow_growth=True
 
+  #tfセッションの作成、モデルの生成
   with tf.Session(config=run_config) as sess:
     if FLAGS.dataset == 'mnist':
       dcgan = DCGAN(
