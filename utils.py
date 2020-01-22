3d8c13d874fbff3f51c6959a8e68fbd50e99c855
hamusita <meazero965@gmail.com>
2020-01-22 09:39:19 +0900

    eg

--- utils.py
+++ utils.py
@@ -11,6 +11,8 @@ import numpy as np
 from time import gmtime, strftime
 from six.moves import xrange
 
+import json
+
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
@@ -176,9 +178,16 @@ def visualize(sess, dcgan, config, option):
     save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_%s.png' % strftime("%Y-%m-%d-%H-%M-%S", gmtime()))
   elif option == 1:
     values = np.arange(0, 1, 1./config.batch_size)
+
+    with open('./samples/z.json', 'w') as f:
+      json.dump({"z" : "Latent variable"}, f, indent=2)
+
+    d_up = {}
+
     for idx in xrange(dcgan.z_dim):
       print(" [*] %d" % idx)
       z_sample = np.random.uniform(-1, 1, size=(config.batch_size , dcgan.z_dim))
+
       for kdx, z in enumerate(z_sample):
         z[idx] = values[kdx]
 
@@ -191,7 +200,13 @@ def visualize(sess, dcgan, config, option):
       else:
         samples = sess.run(dcgan.sampler, feed_dict={dcgan.z: z_sample})
 
+      d_up['./samples/test_arange_%s.png' % (idx)] = z_sample.tolist()
+
       save_images(samples, [image_frame_dim, image_frame_dim], './samples/test_arange_%s.png' % (idx))
+    
+    with open('./samples/z.json', 'w') as f:
+      json.dump(d_up, f, indent=2)
+              
   elif option == 2:
     values = np.arange(0, 1, 1./config.batch_size)
     for idx in [random.randint(0, dcgan.z_dim - 1) for _ in xrange(dcgan.z_dim)]:
